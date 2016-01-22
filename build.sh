8191ba5d98ae5c2a290f26e0ca171fa75e3fddc4
Sudeep Parajuli <sudeep.parajuli254@gmail.com>
2016-01-22 11:35:25 +0545

    change in build file

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@ if [ ! -e .env ]; then echo ".env file does not exist. Please copy the .env file
 sh check_web_server.sh
 git clone git@gitlab.yipl.com.np:web-apps/rc-country.git rc-country
 mv .env rc-country/
+echo "please enter the primary color in hexadecimal form"
+read f1
+if [ "$f1" != "" ] 
+then 
+sed -i "s/#17A9A8/$f1/g" rc-country/public/css/color-country-view.css
+fi
+echo "please enter the secondary color in hexadecimal form"
+read f2
+if [ "$f1" != "" ]
+then 
+sed -i "s/#0B9C9B/$f2/g" rc-country/public/css/color-country-view.css
+fi
 docker build -t rc-country:sierraleone .
 hasid=`docker ps -a|grep -i rc-country-sierraleone|awk '{print $1}'`
 if [ $hasid ]; then docker rm -f rc-country-sierraleone; fi                                    
